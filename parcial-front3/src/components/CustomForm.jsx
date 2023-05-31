d55cf6a62f3c336a43d03bfcffa5c77434c1011f
manesp <mespina98@gmail.com>
2023-05-31 12:17:59 -0300

    template parcial

---
+++ parcial-front3/src/components/CustomForm.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const CustomForm = () => {
+  return (
+    <>
+    <form action="">
+        <select name="select" id="select">
+            <option value=""></option>
+            <option value=""></option>
+            <option value=""></option>
+            <option value=""></option>
+            <option value=""></option>
+        </select>
+        <button></button>
+    </form>
+    </>
+  )
+}
+
+
+
+export default CustomForm
